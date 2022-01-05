0a204b8815f6ef78e9fc769d6fd1057ca18cbac9
Komgris.R <komgris.r@kisracorp.com>
2022-01-05 17:29:22 +0700

    complete enemy sekk

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -11,10 +11,12 @@ enum{
 	CHASE
 }
 var velocity = Vector2.ZERO
-var state = IDLE
+var state = CHASE
 var knockback =Vector2.ZERO
 
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO,FRICTION*delta)
@@ -23,13 +25,24 @@ func _physics_process(delta):
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO,200*delta)
+			seek_player()
 		WANDER:
 			pass
 		CHASE:
-			pass
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction  = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction*MAX_SPEED, ACCELERATION*delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+	
+	
+	velocity = move_and_slide(velocity)
 			
 func seek_player():
-	pass
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
--- Enemies/PlayerDetection.gd
+++ Enemies/PlayerDetection.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 var player = null 
 
+func can_see_player():
+	return player != null
 
 func _on_PlayerDetection_body_entered(body):
 	player=body
